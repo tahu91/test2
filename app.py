eadf66dbfd03cca52995d8d847470468bf6d8476
tahu91 <153413173+tahu91@users.noreply.github.com>
2025-05-02 22:25:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png
 if uploaded_file:
     # Baca gambar yang diupload
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption='Uploaded Image.', use_container_width=True)  # Gunakan use_container_width
 
     # Preprocessing gambar
     processed_image = preprocess_image(image)
