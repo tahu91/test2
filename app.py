f54f86044769d71a4c43fbdc3a1b579aca6f4f20
tahu91 <153413173+tahu91@users.noreply.github.com>
2025-05-03 09:28:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def main():
     with st.sidebar:
         st.header("Pengaturan")
         show_confidence = st.checkbox("Tampilkan Visualisasi Confidence", True)
-        # debug_mode = st.checkbox("Mode Debug", False)
+        debug_mode = False
 
     # Upload gambar
     uploaded_file = st.file_uploader(
